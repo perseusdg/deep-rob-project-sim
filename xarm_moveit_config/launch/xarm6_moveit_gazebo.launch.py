266b3ef8a049d0f84de4506f9278a19a6793c721
perseusdg <harshvardhan.chandira@gmail.com>
2024-12-08 15:49:15 -0600

    adjust table  and arm positons

--- xarm_moveit_config/launch/xarm6_moveit_gazebo.launch.py
+++ xarm_moveit_config/launch/xarm6_moveit_gazebo.launch.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2021, UFACTORY, Inc.
-# All rights reserved.
-#
-# Author: Vinman <vinman.wen@ufactory.cc> <vinman.cub@gmail.com>
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
@@ -12,12 +6,10 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
-
 def generate_launch_description():
     hw_ns = LaunchConfiguration('hw_ns', default='xarm')
     
     # robot moveit gazebo launch
-    # xarm_moveit_config/launch/_robot_moveit_gazebo.launch.py
     robot_moveit_gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(PathJoinSubstitution([FindPackageShare('xarm_moveit_config'), 'launch', '_robot_moveit_gazebo.launch.py'])),
         launch_arguments={
@@ -25,10 +17,13 @@ def generate_launch_description():
             'robot_type': 'xarm',
             'hw_ns': hw_ns,
             'no_gui_ctrl': 'false',
-            'add_realsense_d435i':'true',
-            'add_gripper':'true',
-            'realsense_attach_to': 'world',  # Attach RealSense to world
-            'realsense_attach_xyz': '0 0 1.5',  # Position above the table (adjust as needed)
+            'add_realsense_d435i': 'true',
+            'add_gripper': 'true',
+            'attach_to': 'world',
+            'attach_xyz': '-0.4 0.5 0.0',  # Move to other side: +X forward, -Y across table
+            'attach_rpy': '0.0 0.0 1.5',  # Rotate 180 degrees around Z axis (in radians)
+            'realsense_attach_to': 'world',
+            'realsense_attach_xyz': '0 0 1.5',
             'realsense_attach_rpy': '0 0 0'
         }.items(),
     )
